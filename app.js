7dd4335bd0629d5ca7cd2f90384cdd7f18c4e084
tolstovr <tolstovrob@gmail.com>
2024-10-13 09:12:03 +0400

    feat: close navbar on navlink click on mobile devices

--- app.js
+++ app.js
@@ -1,18 +1,29 @@
 // Navbar toggle
+const navbarToggle = document.getElementById('nav-toggle');
+const navbarItems = document.querySelectorAll('nav > .items-wrapper');
 
-const navbarToggle = document.getElementById("nav-toggle");
-const navbarItems = document.querySelectorAll("nav > .items-wrapper");
-
-navbarToggle.addEventListener("click", () => {
-  if (navbarToggle.dataset.active === "true") {
-    navbarToggle.dataset.active = "false";
-    navbarItems.forEach((item) => item.classList.add("hidden"));
-    navbarToggle.children[0].classList.remove("bi-x-lg");
-    navbarToggle.children[0].classList.add("bi-list");
+navbarToggle.addEventListener('click', () => {
+  if (navbarToggle.dataset.active === 'true') {
+    navbarToggle.dataset.active = 'false';
+    navbarItems.forEach((item) => item.classList.add('hidden'));
+    navbarToggle.children[0].classList.remove('bi-x-lg');
+    navbarToggle.children[0].classList.add('bi-list');
   } else {
-    navbarToggle.dataset.active = "true";
-    navbarItems.forEach((item) => item.classList.remove("hidden"));
-    navbarToggle.children[0].classList.add("bi-x-lg");
-    navbarToggle.children[0].classList.remove("bi-list");
+    navbarToggle.dataset.active = 'true';
+    navbarItems.forEach((item) => item.classList.remove('hidden'));
+    navbarToggle.children[0].classList.add('bi-x-lg');
+    navbarToggle.children[0].classList.remove('bi-list');
   }
+});
+
+const navbarNavlinks = document.querySelectorAll('nav > .items-wrapper > .items.navlinks');
+navbarNavlinks.forEach((item) => {
+  item.addEventListener('click', () => {
+    if (navbarToggle.dataset.active === 'true') {
+      navbarToggle.dataset.active = 'false';
+      navbarItems.forEach((i) => i.classList.add('hidden'));
+      navbarToggle.children[0].classList.remove('bi-x-lg');
+      navbarToggle.children[0].classList.add('bi-list');
+    }
+  });
 });
